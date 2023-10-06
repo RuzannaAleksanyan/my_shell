0e1427d0770e6e7acbeadea22d700a4d22a5b289
RozAle <110930300+RuzannaAleksanyan@users.noreply.github.com>
2023-10-06 18:37:53 +0400

    Create shell.h

---
+++ shell.h
@@ -0,0 +1,44 @@
+#ifndef SHELL
+#define SHELL
+
+#include "job.h"
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <dirent.h>
+
+#include <time.h> // Include this header for localtime and asctime
+#include <stdio.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <fcntl.h> // Include this header for file-related constants
+#include <string.h>
+#include <sys/stat.h>
+#include <pwd.h>
+#include <grp.h>
+
+
+#define MAX_INPUT_SIZE 1024
+#define MAX_ARGS 100
+#define PATH_MAX 4096
+
+void clear_terminal();
+void help();
+void function(const char* command);
+void get_current_directory(char *buf, size_t size);
+int create_directory(const char *path);
+int remove_file(const char *filename);
+int change_directory(const char *path);
+void echo(char* message[], int arg_count);
+void sigint_handler();
+int ping(char *command);
+void run_pipeline(char **commands);
+void list_files_detailed(const char *path);
+int append_output_to_file(const char *command, const char *filename);
+void execute_command(char* args[], int arg_count);
+void pipe_commands(const char *cmd1, const char *cmd2);
+void run_shell();
+
+#endif 
