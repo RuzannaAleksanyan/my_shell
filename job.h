1ad7c06c8b6bacda0b201e314ad5e757c971e5ab
RozAle <110930300+RuzannaAleksanyan@users.noreply.github.com>
2023-10-06 18:35:17 +0400

    Create job.h

---
+++ job.h
@@ -0,0 +1,28 @@
+#ifndef JOB
+#define JOB
+
+#include <fcntl.h> // Include this header for file-related constants
+#include <string.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+
+#define MAX_JOBS 10
+#define MAX_CMD_LENGTH 1024
+
+// Structure to store job information
+typedef struct {
+    pid_t pid;
+    char command[MAX_CMD_LENGTH];
+} Job;
+
+void add_job(pid_t pid, const char *command);
+void list_jobs();
+void run_command_in_background(const char* command);
+void remove_job(int index);
+void bring_to_foreground(pid_t pid);
+void resume_background_job(pid_t pid);
+
+#endif
