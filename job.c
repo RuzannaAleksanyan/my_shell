6211565d3546795a29217f363310baddb26b5e27
Roz Ale <110930300+RuzannaAleksanyan@users.noreply.github.com>
2023-10-13 15:29:12 +0400

    Update job.c

--- job.c
+++ job.c
@@ -17,15 +17,12 @@ void add_job(pid_t pid, const char *command) {
 
 // command &
 void run_command_in_background(const char* command) {
-    // Fork a child process
     pid_t pid = fork();
     
     if (pid == -1) {
         perror("fork");
         exit(1);
     } else if (pid == 0) {
-        // Child process
-        // Redirect standard error to /dev/null
         int devnull = open("/dev/null", O_WRONLY);
         if (devnull == -1) {
             perror("open");
@@ -33,8 +30,7 @@ void run_command_in_background(const char* command) {
         }
         dup2(devnull, STDERR_FILENO);
         close(devnull);
-
-        // Execute the command in the background
+        
         char* args[] = { (char*)command, NULL };
         if (execvp(args[0], args) == -1) {
             perror("execvp"); // This will not be displayed
@@ -51,7 +47,6 @@ void remove_job(int index) {
         return;
     }
     
-    // Overwrite the job to be removed with the last job in the list
     jobs[index] = jobs[num_jobs - 1];
     num_jobs--;
 }
@@ -78,7 +73,6 @@ void resume_background_job(pid_t pid) {
     for (int i = 0; i < num_jobs; ++i) {
         if (jobs[i].pid == pid) {
 
-            // Remove the job from the list
             for (int j = i; j < num_jobs - 1; j++) {
                 jobs[j] = jobs[j + 1];
             }
@@ -93,7 +87,6 @@ void resume_background_job(pid_t pid) {
             return;
         }
     }
-    // If the PID is not found in the job list
     printf("Job with PID %d not found.\n", pid);
 }
 
@@ -103,17 +96,12 @@ void jobss() {
         pid_t pid = jobs[i].pid;
 
         if (waitpid(pid, &status, WNOHANG) == 0) {
-            // The job is still running
             printf("[%d]+   Running       %s (PID %d)\n", i + 1, jobs[i].command, pid);
         } else if (WIFEXITED(status)) {
-            // The job has exited
             printf("[%d]+   Done          %s (PID %d)\n", i + 1, jobs[i].command, pid);
-            // Remove the job from the list
             remove_job(i);
         } else if (WIFSIGNALED(status)) {
-            // The job was terminated by a signal
             printf("[%d]+   Terminated    %s (PID %d)\n", i + 1, jobs[i].command, pid);
-            // Remove the job from the list
             remove_job(i);
         }
     }
